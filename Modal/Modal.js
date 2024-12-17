14506090f12a4820caf2899dfa69a2c20fd580d0
Jean Menezes da Silva <jeanms.info@gmail.com>
2024-12-17 19:50:23 -0300

    Desenvolvimento de um modal apenas com JavaScript, CSS e HTML.

---
+++ Modal/Modal.js
@@ -0,0 +1,9 @@
+function abrirModal() {
+    document.getElementById('modalOverlay').style.display = 'block';
+    document.getElementById('modal').style.display = 'block';
+}
+
+function fecharModal() {
+    document.getElementById('modalOverlay').style.display = 'none';
+    document.getElementById('modal').style.display = 'none';
+}
